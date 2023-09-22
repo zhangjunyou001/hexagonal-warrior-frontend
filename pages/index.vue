19f041190a2a9c76ac8324a322fae305000a56a2
zhang.charles <xingwei.zhang@outlook.com>
2023-09-22 18:40:24 +0800

    update issue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <div v-swiper:mySwiper="swiperOption">
         <div class="swiper-wrapper">
 
-            <div v-for="banner in bannerList" :key="banner.id" class="swiper-slide" style="background: #040B1B;">
+        <div v-for="banner in bannerList" :key="banner.id" class="swiper-slide" style="background: #040B1B;">
                 <a target="_blank" :href="banner.linkUrl">
                     <img :src="banner.imageUrl" :alt="banner.title">
                 </a>
@@ -13,6 +13,93 @@
         <div class="swiper-pagination swiper-pagination-white"></div>
         <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
         <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+    </div>
+
+    <div id="aCoursesList">
+      <div>
+        <section class="container">
+          <header class="comm-title">
+            <h2 class="tac">
+              <span class="c-333">Hot Course</span>
+            </h2>
+          </header>
+          <div>
+            <article class="comm-course-list">
+              <ul class="of" id="bna">
+                <li v-for="course in eduList" :key="course.id">
+                  <div class="cc-l-wrap">
+                    <section class="course-img">
+                      <img
+                        :src="course.cover"
+                        class="img-responsive"
+                        :alt="course.title"
+                      >
+                      <div class="cc-mask">
+                        <a href="#" title="Start learn" class="comm-btn c-btn-1">Start learn</a>
+                      </div>
+                    </section>
+                    <h3 class="hLh30 txtOf mt10">
+                      <a href="#" :title="course.title" class="course-title fsize18 c-333">{{course.title}}</a>
+                    </h3>
+                    <section class="mt10 hLh20 of">
+                      <span class="fl jgAttr c-ccc f-fA">
+                        <i class="c-999 f-fA">9634 learn</i>
+                        |
+                        <i class="c-999 f-fA">9634 comments</i>
+                      </span>
+                    </section>
+                  </div>
+                </li>
+
+              </ul>
+              <div class="clear"></div>
+            </article>
+            <section class="tac pt20">
+              <a href="#" title="All course" class="comm-btn c-btn-2">All course</a>
+            </section>
+          </div>
+        </section>
+      </div>
+      <div>
+        <section class="container">
+          <header class="comm-title">
+            <h2 class="tac">
+              <span class="c-333">Famous Teacher</span>
+            </h2>
+          </header>
+          <div>
+            <article class="i-teacher-list">
+              <ul class="of">
+                <li v-for="teacher in teacherList" :key="teacher.id">
+                  <section class="i-teach-wrap">
+                    <div class="i-teach-pic">
+                      <a href="/teacher/1" :title="teacher.name">
+                        <img :alt="teacher.name" :src="teacher.avatar">
+                      </a>
+                    </div>
+                    <div class="mt10 hLh30 txtOf tac">
+                      <a href="/teacher/1" :title="teacher.name" class="fsize18 c-666">{{teacher.name}}</a>
+                    </div>
+                    <div class="hLh30 txtOf tac">
+                      <span class="fsize14 c-999">{{teacher.career}}</span>
+                    </div>
+                    <div class="mt15 i-q-txt">
+                      <p
+                        class="c-999 f-fA"
+                      >{{teacher.intro}}</p>
+                    </div>
+                  </section>
+                </li>
+
+              </ul>
+              <div class="clear"></div>
+            </article>
+            <section class="tac pt20">
+              <a href="#" title="All Teachers" class="comm-btn c-btn-2">All Teachers</a>
+            </section>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -46,7 +133,7 @@ export default {
     getHotCourseTeacher() {
       index.getIndexData()
         .then(response => {
-          this.eduList = response.data.data.eduList
+          this.eduList = response.data.data.courseList
           this.teacherList = response.data.data.teacherList
         })
     },
