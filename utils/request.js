2925147bac54ee2aae2a4ff440443a56fe57f610
zhang.charles <xingwei.zhang@outlook.com>
2023-09-22 16:24:03 +0800

    update host

--- utils/request.js
+++ utils/request.js
@@ -3,7 +3,7 @@ import cookie from 'js-cookie'
 import { MessageBox, Message } from 'element-ui'
 
 const service = axios.create({
-  baseURL: `http://44.203.136.211:8222`, // api的base_url
+  baseURL: `http://184.73.109.76:8222`, // api的base_url
   timeout: 20000 // 请求超时时间
 })
 service.interceptors.request.use(
